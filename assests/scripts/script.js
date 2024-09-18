b11697d2065f5fe0ab317909de7dc444824b8486
Code masters <pankajhadole24@gmail.com>
2024-09-18 22:16:38 +0530

    Initial commit

---
+++ assests/scripts/script.js
@@ -0,0 +1,19 @@
+// navbar toggle
+const menuToggle = document.getElementById('menuToggle');
+const navContainer = document.getElementById('navContainer');
+
+function toggleMenu() {
+    navContainer.classList.toggle('open');
+    menuToggle.classList.toggle('open');
+    document.body.style.overflow = navContainer.classList.contains('open') ? 'hidden' : '';
+}
+
+menuToggle.addEventListener('click', toggleMenu);
+
+document.addEventListener('click', function(event) {
+    if (!navContainer.contains(event.target) && event.target !== menuToggle && !menuToggle.contains(event.target)) {
+        navContainer.classList.remove('open');
+        menuToggle.classList.remove('open');
+        document.body.style.overflow = '';
+    }
+});
