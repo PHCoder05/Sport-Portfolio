49f98b7ade97491ebab4e9b71b3d24ba2ace31c8
Code masters <pankajhadole24@gmail.com>
2024-09-22 23:34:08 +0530

    update final one

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -18,8 +18,47 @@ document.addEventListener('click', function(event) {
     }
 });
 
-// prevent right clicks 
-document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-  });
+// // prevent right clicks 
+// document.addEventListener('contextmenu', function(event) {
+//     event.preventDefault();
+//   });
+// Show button when user scrolls down 20px from the top of the document
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("scrollTopBtn").classList.remove("d-none");
+  } else {
+    document.getElementById("scrollTopBtn").classList.add("d-none");
+  }
+};
 
+// Scroll to the top of the document
+function scrollToTop() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
+// scroll review
+$(document).ready(function(){
+    $('.testimonials-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
