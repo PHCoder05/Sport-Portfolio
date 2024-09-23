eaf94acbc18893a9f79b7048944eafcac05589a3
Code masters <pankajhadole24@gmail.com>
2024-09-23 18:52:29 +0530

    added animation and smooth scrolling effect

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -62,3 +62,28 @@ $(document).ready(function(){
         ]
     });
 });
+
+$(document).ready(function(){
+    $('.logo-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
